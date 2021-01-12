b385f0fa6c4a342ce76d79a7609ef446868c3931
Cothrax <clarklee.me@gmail.com>
2021-01-12 10:04:52 +0800

    rm run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from cfr_py.naive_cfr import *
-
-if __name__ == '__main__':
-    # filename = 'cfr_py/pncfr20.dat'  # naive cfr file path
-    # test_generator(filename)
-    test_randomized_ncfr()
